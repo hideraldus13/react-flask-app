e702f858dc1b637fb6b6ae719cc794239f0aaeae
Hideraldo Luís <hideraldus13@gmail.com>
2020-12-06 15:51:41 -0300

    Subindo o código

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react-flask-app
+
+Teste utilizando React e Flask. 
+
+Este código foi desenvolvido com base no tutorial abaixo:
+https://blog.miguelgrinberg.com/post/how-to-create-a-react--flask-project
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>A hora atual é {currentTime}.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
